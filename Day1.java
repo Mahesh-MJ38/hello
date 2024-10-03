97e009c4c52fbee5b2d385c119760ced5ab03978
JaladrakshiniMahesh <maheshjakkula555@gmail.com>
2024-10-03 17:45:26 +0530

    spreading gossips hahah...

--- Day1.java
+++ Day1.java
@@ -3,5 +3,6 @@ public class Day1
     public static void main(String[] args) {
         
         System.out.println("Hello Jaladrakshini...");
+        System.out.println("Mahesh is in lopve with Jaladrakshini...");
     }
 }
